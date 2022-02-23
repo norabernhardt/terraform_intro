15c9006b05f42b7b0ec5ef7bec6976315149e5e0
nora bernhardt <noraghasel@gmail.com>
2022-02-23 08:50:24 +0100

    Add instance and security group

--- vpc.tf
+++ vpc.tf
@@ -24,5 +24,35 @@ resource "aws_route_table" "routetable" {
 resource "aws_route_table_association" "routetable-association" {
   route_table_id = aws_route_table.routetable.id
   subnet_id = aws_subnet.public-subnet.id
-
 } 
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow http traffic"
+  vpc_id      = aws_vpc.webserver.id
+
+  ingress {
+    description = "Http"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_instance" "webserver" {
+  ami                         = "ami-033b95fb8079dc481"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public-subnet.id
+  associate_public_ip_address = true
+  security_groups             = [aws_security_group.allow_http.id]
+  user_data                   = file("userdata.sh")
+}
