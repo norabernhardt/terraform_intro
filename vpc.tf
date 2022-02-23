1b91236c2dcd56e638343475c193976ff869f6fd
nora bernhardt <noraghasel@gmail.com>
2022-02-23 08:46:37 +0100

    Add routetable and internet gateway

--- vpc.tf
+++ vpc.tf
@@ -8,3 +8,21 @@ resource "aws_subnet" "public-subnet" {
     cidr_block = "10.1.1.0/24"
     availability_zone = "us-east-1a"
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.webserver.id
+}
+
+resource "aws_route_table" "routetable" {
+  vpc_id = aws_vpc.webserver.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+} 
+
+resource "aws_route_table_association" "routetable-association" {
+  route_table_id = aws_route_table.routetable.id
+  subnet_id = aws_subnet.public-subnet.id
+
+} 
